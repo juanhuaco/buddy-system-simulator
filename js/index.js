a8c349b67336a5aa2a8655e89cf6372a22eb7628
juanhuaco <juanpablohuacodelcarpio@gmail.com>
2022-03-10 09:56:32 -0300

    agregu[e comentarios simplemente

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 import View from './view.js';
 import Model from './model.js';
 
+//la idea de esta implementacion es usar un fenwick tree para simular el sistema compa;
+//cada slot representara un div con col-6 y cada slot ser[a un nuevo sistema de columnas]
+
 document.addEventListener('DOMContentLoaded', ()=>{
     const view = new View();
     const model = new Model();
